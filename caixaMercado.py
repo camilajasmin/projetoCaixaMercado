111e8f71f955c1bf0853d8dfed31c19b954ef13a
camila jasmin <camilajasmin.ec@gmail.com>
2024-02-28 12:03:10 -0300

    Criação de layouts para organização

--- caixaMercado.py
+++ caixaMercado.py
@@ -8,9 +8,34 @@ class CaixaMercado(QWidget):
         super().__init__()
         self.setGeometry(0,25,1590,840)
         self.setWindowTitle("Caixa Mercado")
+        
+        layoutCabecalho  = QHBoxLayout()
+        layoutDireita    = QVBoxLayout()
+        layoutEsquerda   = QVBoxLayout()
+        layoutImgDados   = QHBoxLayout()
+        layoutDadosProd  = QVBoxLayout()
+        layoutLegenda    = QHBoxLayout()
 
-        verticalEsquerda = QVBoxLayout()
-        verticalDireita = QVBoxLayout()
+        layoutTbValores  = QVBoxLayout()
+        layoutTtlTroco   = QHBoxLayout()
+
+        layoutTelaHor    = QHBoxLayout()
+        layoutTelaVer    = QVBoxLayout()  
+
+        labelCabecalho = QLabel("CAIXA ABERTO!")
+        labelCabecalho.setStyleSheet("QLabel{color:white;background-color:#836FFF;font-size:40px}")
+        labelCabecalho.setFixedHeight(100)
+
+        labelEsquerda  = QLabel()
+        labelEsquerda.setFixedWidth(800)
+        labelEsquerda.styleSheet("QLabel{background-color:}")
+
+        # 
+        labelCabecalho.setLayout(layoutCabecalho)
+        layoutTelaVer.addWidget(labelCabecalho)
+        #Tela Inteira
+        self.setLayout(layoutTelaVer)
+        
 
 
 
